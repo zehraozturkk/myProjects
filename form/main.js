1a5692b6d03ea2e28bcb332a10d8c568cfc8cacf
zehraozturkk <fzehraozturk34@gmail.com>
2023-02-18 16:44:52 +0300

    final commit

--- form/main.js
+++ form/main.js
@@ -16,43 +16,60 @@ function success(input){
     input.className ='form-control success'
 }
 
-const validateEmail = (email) => {
-    return String(email)
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      );
+const checkEmail = (input) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      
+    if(re.test(input.value)){
+        success(input);
+    }else{
+        error(input, 'email is required')
+    }
   };
+
+function checkRequired(inputs) {
+    inputs.forEach((input) => {
+        if(input.value===''){
+            error(input, `${input.id} is required` );
+        }else{
+            success(input);
+        }
+    })
+    
+}
+
+function checkLenght(input, min, max){
+    if(input.value.lenght < min){
+        error(input, `${input.id} en az ${min} karakterli olmali.`)
+    }else if(input.value.lenght > max){
+        error(input, `${input.id} en fazla ${max} karakterli olmali.`)
+    }else{
+        success(input)
+    }
+}
+
+function checkPasswords(input1,input2){
+    if(input1.value !== input2.value){
+        error(input2,'parolalar eşleşmiyor')
+    }
+}
+
+const checkPhone =(input)=>{
+    var exp = /^\d{10}$/;
+    if(!exp.test(input.value)){
+        error(input, 'phone must be 10 character')
+    }
+}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault(); // bunun sayesinde yenilenmeyi engellemiş olduk.
 
-    if(username.value === ''){  
-        error(username, 'username is needed')
-    }else{
-        success(username)
-    }
-    if(email.value === ''){
-        error(email, 'email is needed')
-    }else if(!validateEmail(email.value)){//eğer burdan true bir mail gelirse bu kısmı direkt atlar
-        error(email, 'please, you write a correct email adress')
-    }
-    else{
-        success(email)
-    }
-    if(phone.value === ''){
-        error(phone, 'phone is needed')
-    }else{
-        success(phone)
-    }
-    if(password.value === ''){
-        error(password, 'password is needed')
-    }else{
-        success(password)
-    }
-    if(rePassword.value === ''){
-        error(rePassword, 're-password is needed')
-    }else{
-        success(rePassword)
-    }
+    checkRequired([username, email, phone, password, rePassword]);
+    checkEmail(email)
+    checkLenght(username,7,14);
+    checkLenght(password,7,12);
+    checkPasswords(password,rePassword);
+    checkPhone(phone)
+    
+
+   
 })
